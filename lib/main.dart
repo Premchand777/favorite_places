2cf005c736a2cd102dd217f588c6510544bc75f2
Premchand777 <65839523+Premchand777@users.noreply.github.com>
2024-03-31 13:04:28 +0530

    initial commit and setup

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+final colorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 6, 74, 247),
+  background: const Color.fromARGB(180, 6, 0, 14),
+);
+
+final theme = ThemeData().copyWith(
+  scaffoldBackgroundColor: colorScheme.background,
+  colorScheme: colorScheme,
+  textTheme: GoogleFonts.ubuntuCondensedTextTheme().copyWith(
+    titleSmall: GoogleFonts.ubuntuCondensed(
+      fontWeight: FontWeight.bold,
+    ),
+    titleMedium: GoogleFonts.ubuntuCondensed(
+      fontWeight: FontWeight.bold,
+    ),
+    titleLarge: GoogleFonts.ubuntuCondensed(
+      fontWeight: FontWeight.bold,
+    ),
+  ),
+);
+
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: MaterialApp(
+        title: 'Favorite Places',
+        theme: theme,
+        home: const Scaffold(
+          body: Center(
+            child: Text('Favorite Places'),
+          ),
+        ),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
